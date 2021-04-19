0ecc043eb2a3a8d491802d5aa298b7000b42fcef
smarthug <kirklayer@gmail.com>
2021-04-19 16:13:54 +0900

    inline test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import * as Pages from './page'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+
+const labs = Object.entries(Pages);
+// console.log(labs);
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <div style={{ height: "100vh" }} className="App">
+        <Switch>
+          
+          {labs.map(([k, v]) => {
+            if (v.default) {
+              const Tmp = v.default
+              return (
+                <Route
+                  exact
+                  key={k}
+                  path={`/${k}`}
+                >
+                  <Tmp />
+                </Route>
+              )
+            }
+
+            return null
+          })}
+          <Route
+            exact
+            path={`/`}
+          >
+            <h1 style={{marginTop:0}}>LAB</h1>
+            {labs.map(([k, v]) => {
+              if (v.default) {
+                return (
+                  <div key={k}>
+                    <Link to={`/${k}`}>
+                      {k}
+                    </Link>
+                  </div>
+                )
+              }
+
+              return null
+            })}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/page/base.js
@@ -0,0 +1,63 @@
+import * as THREE from "three";
+import React, { useEffect, useRef } from "react";
+
+import CameraControls from "camera-controls";
+import { ARButton } from './inlineButton'
+
+CameraControls.install({ THREE: THREE });
+
+let cube, scene, camera, renderer, cameraControls;
+const clock = new THREE.Clock();
+
+export default function Main() {
+    const containerRef = useRef();
+    const canvasRef = useRef();
+    const vrButtonConRef = useRef();
+    useEffect(() => {
+        Init();
+        Animate();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    function Init() {
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+        renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvasRef.current });
+        renderer.xr.enabled = true;
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.xr.setFramebufferScaleFactor(2.0);
+        vrButtonConRef.current.appendChild(ARButton.createButton(renderer));
+
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var material = new THREE.MeshNormalMaterial();
+        cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        camera.position.z = 5;
+
+        cameraControls = new CameraControls(camera, renderer.domElement);
+    }
+
+    function Animate() {
+        requestAnimationFrame(Animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        const delta = clock.getDelta();
+        // const hasControlsUpdated = cameraControls.update(delta);
+        cameraControls.update(delta);
+
+        renderer.render(scene, camera);
+    }
+
+    return <div ref={containerRef}>
+        <canvas ref={canvasRef} />
+        <div ref={vrButtonConRef}></div>
+    </div>;
+}
